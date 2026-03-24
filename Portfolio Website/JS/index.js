ba4dd0725b7549b69fd95c1e21ae714c788e58da
0Lazarus0 <jaimiller08@icloud.com>
2026-03-24 12:56:57 -0400

    Delete Portfolio Website directory

--- Portfolio Website/JS/index.js
+++
@@ -1,6 +0,0 @@
-const logoContainer = document.querySelector('.logo-container');
-const logoMenu = document.querySelector('.logo-menu');
-
-logoContainer.addEventListener('click', () => {
-  logoMenu.classList.toggle('active');
-});
